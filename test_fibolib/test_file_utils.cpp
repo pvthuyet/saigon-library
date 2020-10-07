9b0b7ba6e050f558061f39bc87461b5031736783
pvthuyet <pvthuyet@gmail.com>
2020-10-07 17:34:16 +0700

    update test case

--- test_fibolib/test_file_utils.cpp
+++ test_fibolib/test_file_utils.cpp
@@ -15,21 +15,34 @@
 using namespace fibo::FileUtils;
 using json = nlohmann::json;
 
-std::vector<FileNameInformation<std::string>> readJsonData(std::string fpath)
+template<typename T>
+std::vector<FileNameInformation<T>> readJsonData(std::string fpath)
 {
-	std::vector<FileNameInformation<std::string>> data;
-	std::ifstream ifs("test_data\\file_utils.json");
+	std::vector<FileNameInformation<T>> data;
+	std::ifstream ifs(fpath);
 	json js = json::parse(ifs);
+	std::string str;
 
 	for (auto& e : js[K_PARSE_FILE_NAME])
 	{
-		FileNameInformation<std::string> info;
-		info.mFullPath		= e[K_FULL_PATH];
-		info.mParentPath	= e[K_PARENT];
-		info.mRootPath		= e[K_ROOT];
-		info.mFileName		= e[K_NAME];
-		info.mStem			= e[K_STEM];
-		info.mExtension		= e[K_EXTENSION];
+		FileNameInformation<T> info;
+		str = e[K_FULL_PATH];
+		info.mFullPath		= T(str.begin(), str.end());
+
+		str = e[K_PARENT];
+		info.mParentPath	= T(str.begin(), str.end());
+
+		str = e[K_ROOT];
+		info.mRootPath		= T(str.begin(), str.end());
+
+		str = e[K_NAME];
+		info.mFileName		= T(str.begin(), str.end());
+
+		str = e[K_STEM];
+		info.mStem			= T(str.begin(), str.end());
+
+		str = e[K_EXTENSION];
+		info.mExtension		= T(str.begin(), str.end());
 		data.push_back(std::move(info));
 	}
 	return data;
@@ -37,8 +50,22 @@ std::vector<FileNameInformation<std::string>> readJsonData(std::string fpath)
 
 bool testParseFileName()
 {
-	auto input = readJsonData("test_data\\file_utils.json");
-	for (const auto& pa : input)
+	// test wstring
+	auto input1 = readJsonData<std::wstring>("test_data\\file_utils.json");
+	for (const auto& pa : input1)
+	{
+		auto info = parseFileName(pa.mFullPath);
+		EXPECT_EQ(info.mFullPath, pa.mFullPath);
+		EXPECT_EQ(info.mParentPath, pa.mParentPath);
+		EXPECT_EQ(info.mRootPath, pa.mRootPath);
+		EXPECT_EQ(info.mFileName, pa.mFileName);
+		EXPECT_EQ(info.mStem, pa.mStem);
+		EXPECT_EQ(info.mExtension, pa.mExtension);
+	}
+
+	// test string
+	auto input2 = readJsonData<std::string>("test_data\\file_utils.json");
+	for (const auto& pa : input2)
 	{
 		auto info = parseFileName(pa.mFullPath);
 		EXPECT_EQ(info.mFullPath, pa.mFullPath);
