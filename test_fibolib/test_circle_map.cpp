e8620b864165660137d3207524703a30055a4c8a
tvp <tvp@wherzat.com>
2020-11-15 22:36:21 +0700

    test circle map

--- test_fibolib/test_circle_map.cpp
+++ test_fibolib/test_circle_map.cpp
@@ -6,13 +6,32 @@ constexpr int mapSz = 100;
 using KeyType = std::string;
 struct DataType 
 {
+	DataType() = default;
+	explicit DataType(int v) : value_{ v } {}
 	int value_{};
 	constexpr auto operator<=>(DataType const&) const noexcept = default;
 };
 
+using TestCirMap = fibo::Con::CircleMap<KeyType, DataType, mapSz>;
+
+TestCirMap initMap(int sz = mapSz)
+{
+	using namespace std::string_literals;
+	TestCirMap mp{};
+	for (int i = 0; i < sz; ++i) {
+		std::string key = "key "s + std::to_string(i);
+		DataType v{ i };
+		mp[key] = v;
+	}
+	return mp;
+}
+
 TEST(CircleMap, constructor_fix_size)
 {
-	fibo::Con::CircleMap<KeyType, DataType, mapSz> ciMap{};
+	TestCirMap ciMap;
+	TestCirMap ciMap1 = initMap();
+	ciMap = ciMap1;
+	TestCirMap ciMap3{ std::move(ciMap1) };
 	EXPECT_EQ(mapSz, ciMap.size());
 }
 
@@ -20,14 +39,14 @@ TEST(CircleMap, insert_item)
 {
 	DataType v{};
 	KeyType key = "key 1";
-	fibo::Con::CircleMap<KeyType, DataType, mapSz> ciMap{};
+	TestCirMap ciMap{};
 	ciMap[key] = v;
 	EXPECT_EQ(v, ciMap[key]);
 }
 
 TEST(CircleMap, find_in_empty_circle_map)
 {
-	fibo::Con::CircleMap<KeyType, DataType, mapSz> ciMap{};
+	TestCirMap ciMap{};
 	auto found = ciMap.find("key 1");
 	EXPECT_TRUE(not found);
 }
@@ -35,7 +54,7 @@ TEST(CircleMap, find_in_empty_circle_map)
 TEST(CircleMap, find_available_item)
 {
 	DataType v{};
-	fibo::Con::CircleMap<KeyType, DataType, mapSz> ciMap{};
+	TestCirMap ciMap{};
 	ciMap["key 1"] = v;
 	auto found = ciMap.find("key 1");
 	EXPECT_EQ(v, *found);
@@ -43,7 +62,7 @@ TEST(CircleMap, find_available_item)
 
 TEST(CircleMap, find_not_found_item)
 {
-	fibo::Con::CircleMap<KeyType, DataType, mapSz> ciMap{};
+	TestCirMap ciMap{};
 	ciMap["key 1"] = DataType{};
 	auto found = ciMap.find("key 2");
 	EXPECT_TRUE(not found);
@@ -52,7 +71,7 @@ TEST(CircleMap, find_not_found_item)
 TEST(CircleMap, find_if_available_item)
 {
 	DataType v{};
-	fibo::Con::CircleMap<KeyType, DataType, mapSz> ciMap{};
+	TestCirMap ciMap{};
 	ciMap["key 1"] = v;
 	auto found = ciMap.find_if([&](auto const& k, auto const& val) {
 		return val == v;
